314a63bf1d2a993436b76db290d9533658b6b4c5
MKody <im@kdy.ch>
2017-03-19 16:47:29 +0000

    added HTML grid and .xlsx

---
+++ gentables.py
@@ -0,0 +1,128 @@
+import os
+import requests
+import xlsxwriter
+
+from jinja2 import Environment, FileSystemLoader
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+times = [
+    '6:00 am',
+    '6:15 am',
+    '6:30 am',
+    '6:45 am',
+    '7:00 am',
+    '7:15 am',
+    '7:30 am',
+    '7:45 am',
+    '8:00 am',
+    '8:15 am',
+    '8:30 am',
+    '8:45 am',
+    '9:00 am',
+    '9:15 am',
+    '9:30 am',
+    '9:45 am',
+    '10:00 am',
+    '10:15 am',
+    '10:30 am',
+    '10:45 am',
+    '11:00 am',
+    '11:15 am',
+    '11:30 am',
+    '11:45 am',
+    '12:00 pm',
+    '12:15 pm',
+    '12:30 pm',
+    '12:45 pm',
+    '1:00 pm',
+    '1:15 pm',
+    '1:30 pm',
+    '1:45 pm',
+    '2:00 pm',
+    '2:15 pm',
+    '2:30 pm',
+    '2:45 pm',
+    '3:00 pm',
+    '3:15 pm',
+    '3:30 pm',
+    '3:45 pm',
+    '4:00 pm',
+    '4:15 pm',
+    '4:30 pm',
+    '4:45 pm',
+    '5:00 pm',
+    '5:15 pm',
+    '5:30 pm',
+    '5:45 pm',
+    '6:00 pm',
+    '6:15 pm',
+    '6:30 pm',
+    '6:45 pm',
+    '7:00 pm',
+    '7:15 pm',
+    '7:30 pm',
+    '7:45 pm',
+]
+
+
+def gen_sheet():
+    r = requests.get('https://api.sug.rocks/ccnschedule.json')
+    cont = r.json()
+
+    j2_env = Environment(loader=FileSystemLoader(os.path.join(THIS_DIR)), trim_blocks=True)
+
+    # Generate page
+    j2_env.get_template('grid.tpl').stream(days=cont, times=times)\
+        .dump(os.path.join(THIS_DIR, 'grid.html'))
+
+    workbook = xlsxwriter.Workbook(os.path.join(THIS_DIR, 'schedule.xlsx'))
+    worksheet = workbook.add_worksheet()
+
+    cell_format = workbook.add_format({'bold': True, 'font_size': 20})
+    worksheet.merge_range(0, 1, 0, 7, 'CARTOON NETWORK SCHEDULE', cell_format)
+
+    row = 2
+    col = 0
+
+    cell_format = workbook.add_format({'bold': True, 'align': 'right'})
+    for time in times:
+        worksheet.write(row, col, time, cell_format)
+        row += 1
+
+    row = 1
+    col = 1
+
+    for day in cont:
+        cell_format = workbook.add_format({'bold': True, 'align': 'center'})
+        worksheet.write(row, col, day, cell_format)
+
+        row += 1
+        for slot in cont[day]:
+            name = slot['show']
+            if name == 'MOVIE' or name == 'SPECIAL:':
+                name = slot['title']
+
+            cell_format = workbook.add_format({
+                'align': 'center',
+                'valign': 'vcenter',
+                'fg_color': slot['color']
+            })
+
+            if slot['slots'] > 1:
+                nrow = row + slot['slots'] - 1
+                worksheet.merge_range(row, col, nrow, col, name, cell_format)
+            else:
+                worksheet.write(row, col, name, cell_format)
+
+            row += slot['slots']
+
+        row = 1
+        col += 1
+
+    worksheet.set_column(1, 50, 24.2)
+    worksheet.freeze_panes(2, 1)
+    workbook.close()
+
+
+if __name__ == '__main__':
+    gen_sheet()
---
+++ grid.tpl
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CN Schedule</title>
+
+    <style>
+        .container {
+            overflow: scroll;
+            width: 100vw;
+            height: 100vh;
+            position: fixed;
+            top: 0;
+            white-space: nowrap;
+            left: 0;
+        }
+
+        .day {
+            display: inline-block;
+            vertical-align: top;
+            width: 150px !important;
+            font-size: 10px;
+        }
+
+        .time-table {
+            display: inline-block;
+            vertical-align: top;
+            font-size: 10px;
+        }
+
+        .time-line {
+            height: 25px;
+            display:flex;
+            align-items: center; /* Vertical center alignment */
+            justify-content: center; /* Horizontal center alignment */
+        }
+
+        .slot {
+            overflow: hidden;
+            text-align: center;
+            -webkit-box-shadow: inset 0px 0px 0px 1px #000;
+            -moz-box-shadow: inset 0px 0px 0px 1px #f00;
+            box-shadow: inset 0px 0px 0px 1px #000;
+            display:flex;
+            align-items: center; /* Vertical center alignment */
+            justify-content: center; /* Horizontal center alignment */
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="time-table">
+        <div>
+            Times
+        </div>
+{% for time in times %}
+        <div class="time-line">
+            <span class="time">{{ time }}</span>
+        </div>
+{% endfor %}
+    </div>
+{% for day in days %}
+    <div class="day">
+        <div>
+            {{ day }}
+        </div>
+{% for show in days[day] %}
+        <div title="{{ show.title }} ({{ show.time }})" class="slot" style="height: {{ show.slots * 25 }}px; background-color: {{ show.color }};">
+            <span class="show">{{ show.show }}</span>
+        </div>
+{% endfor %}
+    </div>
+{% endfor %}
+</div>
+</body>
+</html>
