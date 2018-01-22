4ebf77a954892e56c21bd67fef20d65054a76bab
MKody <im@kdy.ch>
2018-01-22 19:11:17 +0100

    NOW, that's better. Doing it myself instead of vue-chartjs

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -3,23 +3,28 @@
     <div class="stats-resume">
       We have {{ totalBlocks }} time slots with {{ dayStats.length }} shows.
     </div>
-    <table class="top-shows">
-      <thead>
-        <tr>
-          <th>Show</th>
-          <th colspan="2">Slots</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="show in dayStats"
-          :key="show.title">
-          <td>{{ show.title }}</td>
-          <td style="text-align: right;">{{ show.slots }}</td>
-          <td>~{{ show.percentage }}%</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="two-columns-stats">
+      <table class="top-shows">
+        <thead>
+          <tr>
+            <th>Show</th>
+            <th colspan="2">Slots</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="show in dayStats"
+            :key="show.title">
+            <td>{{ show.title }}</td>
+            <td style="text-align: right;">{{ show.slots }}</td>
+            <td>~{{ show.percentage }}%</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="show-chart">
+        <canvas id="chart" width="200" height="200"></canvas>
+      </div>
+    </div>
     <div class="source">
       Source of data: {{ source }}
     </div>
@@ -28,6 +33,8 @@
 
 <script>
 import { parseDate } from '../assets/dates'
+import ColorHash from 'color-hash'
+import Chart from 'chart.js/dist/Chart.min.js'
 
 export default {
   name: 'stats',
@@ -37,7 +44,9 @@ export default {
       totalBlocks: 0,
       dayStats: {},
       source: '',
-      ready: false
+      ready: false,
+      chartObj: undefined,
+      statsCharts: {}
     }
   },
   methods: {
@@ -45,6 +54,16 @@ export default {
       return parseDate(date, true)
     },
     showStats (day) {
+      // Clear data
+      this.statsCharts = {
+        labels: [],
+        datasets: [{
+          backgroundColor: [],
+          data: []
+        }]
+      }
+
+      // Load available sources
       if (day.cn) {
         this.source = 'Cartoon Network'
         this.dayStats = day.cn
@@ -53,11 +72,32 @@ export default {
         this.dayStats = day.zap
       }
 
+      // Count blocks and add to chart
       this.totalBlocks = 0
       this.dayStats.forEach(el => {
+        let colorHash = new ColorHash({ lightness: (el.title % 10) / 10 })
+
         this.totalBlocks += el.slots
+        this.statsCharts.labels.push(el.title)
+        this.statsCharts.datasets[0].data.push(el.slots)
+        this.statsCharts.datasets[0].backgroundColor.push(colorHash.hex(el.title))
       })
 
+      // Load chart
+      this.chartObj = new Chart('chart', {
+        type: 'doughnut',
+        data: this.statsCharts,
+        options: {
+          layout: {
+            padding: 15
+          },
+          legend: {
+            display: false
+          }
+        }
+      })
+
+      // Set as ready
       setTimeout(_ => {
         this.ready = true
       }, 500)
@@ -66,6 +106,7 @@ export default {
   watch: {
     day: function () {
       this.ready = false
+      this.chartObj.destroy() // We need to destroy the chart to re-use it
       this.showStats(this.day)
     }
   },
@@ -89,17 +130,39 @@ export default {
   }
 
   .top-shows {
-    display: inline-block;
     text-align: left;
+    width: 100%;
 
     tr + tr {
       border-top: 1px solid $semi-transparent-black;
     }
   }
 
+  .show-chart {
+    padding: 50px;
+  }
+
   .source {
     font-size: small;
     margin-top: 1em;
+  }
+}
+
+@media screen and (min-width: 630px) {
+  .stats {
+    .two-columns-stats {
+      .top-shows {
+        display: inline-block;
+        width: initial;
+      }
+
+      .show-chart {
+        display: inline-block;
+        padding: 0;
+        vertical-align: top;
+        width: 200px;
+      }
+    }
   }
 }
 </style>
